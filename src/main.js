4f52fb5554a3856c10494aa99f63140402c3ab53
Ashkan <ashkan.github0@gmail.com>
2022-05-09 17:01:35 +0430

    Loginpasss

--- src/main.js
+++ src/main.js
@@ -2,5 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from "firebase/app";
+
+
+// Import the functions you need from the SDKs you need
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALR1fC8GtEbamoG5bKQ-dMx1sIwJjj8FA",
+  authDomain: "fir-project-b6e74.firebaseapp.com",
+  projectId: "fir-project-b6e74",
+  storageBucket: "fir-project-b6e74.appspot.com",
+  messagingSenderId: "635076824259",
+  appId: "1:635076824259:web:9ac3685a9e2c20118fa676"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 createApp(App).use(store).use(router).mount('#app')
---
+++ src/views/MyLogin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>Login Page </h1>
+    <input type="email" placeholder="Email" v-model="email" />
+
+    <input type="password" placeholder="Password" v-model="password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/app";
+
+export default {
+  name: "Mylogin",
+  data() {
+    return {
+      email: "",
+      password: "",
+      initials: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
